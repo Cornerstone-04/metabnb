9f3183ddbc33a790c858016668cf46cb5992f31e
Cornerstone <fortunecornerstone@gmail.com>
2022-11-11 10:03:33 +0100

    feat: Setup folder structure
    This commit -m set ups the project folder structure
    and adds the necessary icons for the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AppRoutes from './routes/Router'
 
 const App = () => {
   return (
     <div>
-      
+      <AppRoutes/>
     </div>
   )
 }
---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import Footer from "./Footer";
+import Modal from "./Modal";
+import Navbar from "./Navbar";
+import Button from "./Button";
+
+export { Button, Footer, Modal, Navbar };
