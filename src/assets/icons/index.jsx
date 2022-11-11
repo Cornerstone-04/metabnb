b9214c85f61212cb59ab4e1e76929423350d2c18
Cornerstone <fortunecornerstone@gmail.com>
2022-11-11 10:08:44 +0100

    feat: Create index file for icons export

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,25 @@
+import Facebook from "./facebook_white.svg";
+import Instagram from "./ig_white.svg";
+import Logo from "./logo_coloured.svg";
+import LogoWhite from "./logo_white.svg";
+import MetaMask from "./metamask_color.svg";
+import MetaMaskWhite from "./metamask.svg";
+import MBToken from "./mbtoken.svg";
+import OpenSea from "./opensea.svg";
+import Setting from "./setting.svg";
+import Twitter from "./twitter_white.svg";
+import Wallet from "./walletconnect_color.svg";
+
+export {
+  Facebook,
+  Instagram,
+  Logo,
+  LogoWhite,
+  MetaMask,
+  MetaMaskWhite,
+  MBToken,
+  OpenSea,
+  Setting,
+  Twitter,
+  Wallet,
+};
